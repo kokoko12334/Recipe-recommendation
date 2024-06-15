2713dabcc487ffca77d5900918d3da78453f40ec
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-06-15 16:04:34 +0900

    docs:설명수정

--- mysite/recipe/utils.py
+++ mysite/recipe/utils.py
@@ -7,7 +7,7 @@ with open('data/ingre_vector.pk', 'rb') as f:
 def cal_vector(ingre, weight):
     n = len(ingre)
     weight_sum = sum(weight)
-    weight_adj = [round(w/weight_sum,4) for w in weight]
+    weight_adj = [round(w / weight_sum, 4) for w in weight]
     matrix = np.zeros((n,1536))
     for i in range(n):
         v = vector[ingre[i]] * weight_adj[i]
@@ -15,4 +15,3 @@ def cal_vector(ingre, weight):
     
     recipe_vector = matrix.sum(axis=0)/n
     return recipe_vector
-
